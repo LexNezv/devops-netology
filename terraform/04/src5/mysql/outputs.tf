abe3f182feb5f2c251298a3ecc4aa017761bb8ba
LexNezv <Lex@Nezv.com>
2023-12-30 19:49:56 +0300

    change sql

--- terraform/04/src5/mysql/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "network_id" {
-  value = yandex_vpc_network.vpc.id
-  }
-output "subnet_id" {
-  value = yandex_vpc_subnet.subnet["ru-central1-a"].id
-  }
-output "vpc_subnet" {
-  value = yandex_vpc_subnet.subnet[*]
-  }
--- terraform/04/src5/mysql/sql.tf
+++ terraform/04/src5/mysql/sql.tf
@@ -8,50 +8,29 @@ terraform {
 }
 
 
-resource "yandex_mdb_mysql_cluster" "<имя_кластера>" {
-  name                = "<имя_кластера>"
-  environment         = "<окружение>"
-  network_id          = "<идентификатор_сети>"
-  version             = "<версия_MySQL>"
-  security_group_ids  = [ "<список_идентификаторов_групп_безопасности>" ]
-  deletion_protection = <защита_от_удаления_кластера>
+resource "yandex_mdb_mysql_cluster" "${var.cluster_name}" {
+  name                = var.cluster_name
+  environment         = "PRESTABLE"
+  network_id          = var.network_id
+  version             = "8.0"
 
   resources {
-    resource_preset_id = "<класс_хоста>"
-    disk_type_id       = "<тип_диска>"
-    disk_size          = "<размер_хранилища_ГБ>"
+    resource_preset_id = "s2.micro"
+    disk_type_id       = "network-ssd"
+    disk_size          = 16
+  }
+
+  mysql_config = {
+    sql_mode                      = "ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION"
+    max_connections               = 100
+    default_authentication_plugin = "MYSQL_NATIVE_PASSWORD"
+    innodb_print_all_deadlocks    = true
+
   }
 
   host {
-    zone             = "<зона_доступности>"
-    subnet_id        = "<идентификатор_подсети>"
-    assign_public_ip = <публичный_доступ_к_хосту>
-    priority         = <приоритет_при_выборе_хоста-мастера>
-    backup_priority  = <приоритет_для_резервного_копирования>
+    zone      = "ru-central1-a"
+    subnet_id = var.network_id
   }
-}
-
-resource "yandex_mdb_mysql_database" "<имя_БД>" {
-  cluster_id = "<идентификатор_кластера>"
-  name       = "<имя_БД>"
-}
-
-resource "yandex_mdb_mysql_user" "<имя_пользователя>" {
-  cluster_id = "<идентификатор_кластера>"
-  name       = "<имя_пользователя>"
-  password   = "<пароль_пользователя>"
-  permission {
-    database_name = "<имя_БД>"
-    roles         = ["ALL"]
-  }
-}
-
-resource "yandex_vpc_network" "<имя_сети>" { name = "<имя_сети>" }
-
-resource "yandex_vpc_subnet" "<имя_подсети>" {
-  name           = "<имя_подсети>"
-  zone           = "<зона_доступности>"
-  network_id     = "<идентификатор_сети>"
-  v4_cidr_blocks = ["<диапазон>"]
 }
 
