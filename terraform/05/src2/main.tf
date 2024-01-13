7f7bbdca4b29462e8c736ad6117d5d33af5acd1f
LexNezv <Lex@Nezv.com>
2024-01-13 19:10:23 +0300

    add s3 backend

--- terraform/05/src2/main.tf
+++ terraform/05/src2/main.tf
@@ -5,6 +5,15 @@ terraform {
     }
   }
   required_version = ">=0.13"
+  backend "s3" {
+ endpoint = "storage.yandexcloud.net"
+ bucket = "tfstate-my"
+ region = "ru-central1"
+ key = "terraform.tfstate"
+ 
+ skip_region_validation = true
+ skip_credentials_validation = true
+ }
 }
 
 provider "yandex" {
