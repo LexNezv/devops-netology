dfed6b8665d29661af5684f48cd7b189b7689022
akorotkov <akorotkov@sila.ru>
2025-05-29 21:14:36 +0300

    k8s8

--- k8s/08/crt/createcrt.sh
+++ k8s/08/crt/createcrt.sh
@@ -1,10 +1,13 @@
+CSRNAME=$(date +%s)alex
+echo $CSRNAME
 openssl genrsa -out user.key 2048
-openssl req -new -key user.key -out user.csr -subj "/CN=Alex"
+openssl req -new -key user.key -out user.csr -subj "/CN=alex"
+# или если нам еще нужны группы -subj "/CN=alex/O=$group1/O=$group2/O=$group3"
 cat <<EOF | kubectl apply -f -
 apiVersion: certificates.k8s.io/v1
 kind: CertificateSigningRequest
 metadata:
-  name: Alex-csr
+  name: $CSRNAME
 spec:
   request: $(cat user.csr | base64 | tr -d '\n')
   signerName: kubernetes.io/kube-apiserver-client
@@ -13,17 +16,23 @@ spec:
   - client auth
 EOF
 kubectl get csr
-kubectl certificate approve Alex-csr
-kubectl get csr Alex-csr -o jsonpath='{.status.certificate}' | base64 -d > user.crt
+kubectl certificate approve $CSRNAME
+kubectl get csr $CSRNAME -o jsonpath='{.status.certificate}' | base64 -d > user.crt
+openssl x509 -in user.crt -text -noout | grep "Subject:"
 
-kubectl config set-credentials Alex \
+kubectl config set-credentials alex \
   --client-key=user.key \
   --client-certificate=user.crt \
   --embed-certs=true
 
-kubectl config set-context Alex-context \
+kubectl config set-context alex-context \
   --cluster=microk8s-cluster \
-  --user=Alex \
+  --user=alex \
 
-kubectl config use-context Alex-context
+kubectl config use-context alex-context
+kubectl config current-context
 kubectl get pods
+
+# Хеши должны быть одинаковые
+openssl x509 -noout -modulus -in user.crt | openssl md5
+openssl rsa -noout -modulus -in user.key | openssl md5
